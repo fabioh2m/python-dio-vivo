64ee703b6d458d2a23b00ff4deb81b90d37e7b23
fabioh2m <fabioh2m@hotmail.com>
2026-01-14 07:53:38 -0300

    curso pythoh vivo
    tipos de variáveis

---
+++ conversao-de-tipos/index.py
@@ -0,0 +1,32 @@
+# preco = 10
+# print (preco)
+
+
+#preco = float(preco)#converti para float
+#print (preco)
+
+
+#print (preco / 1)#converteu para float mas se puser // mantem como int.
+
+
+# preco = 10.5
+# print (preco)
+
+# preco = int(preco)#converti para int
+# print (preco)
+
+# print (str(10.1))#converti de float para string
+# print (float('10.5'))#converti de string para float
+# print (int('50'))#converti de string para int
+
+valor = 10
+valor_str = str(valor)
+print (type(valor), (valor_str))
+
+
+
+
+
+
+
+
